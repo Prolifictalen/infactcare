47556be0f279c1680dd5a5caf02b4eaf3e476901
Astha Singh <95242101+Prolifictalen@users.noreply.github.com>
2024-12-09 21:09:53 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -34,47 +34,48 @@ const Navbar = () => {
       </div>
 
       {/* Desktop Links */}
-      //<div className="nav-links-container">
-        //{menuOptions.map((menu) => (
-        //  <Link
-           // key={menu.text}
-           // to={menu.path}
-           // className={`nav-link ${
-             // location.pathname === menu.path ? "active-link" : ""
-           // }`}
-         // >
-          //  {menu.text}
-         // </Link>
-       // ))}
-     // </div>
+      <div className="nav-links-container">
+        {menuOptions.map((menu) => (
+          <Link
+            key={menu.text}
+            to={menu.path}
+            className={`nav-link ${
+              location.pathname === menu.path ? "active-link" : ""
+            }`}
+          >
+            {menu.text}
+          </Link>
+        ))}
+      </div>
 
       {/* Mobile Menu Button */}
       <div className="nav-menu-container">
         <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
       </div>
 
-  <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
-  <Box
-    sx={{ width: "250px" }}
-    role="presentation"
-    onClick={() => setOpenMenu(false)}
-  >
-    <List>
-      {menuOptions.map((menu) => (
-        <ListItem key={menu.text} disablePadding>
-          <ListItemButton component={Link} to={menu.path}>
-            <ListItemText
-              primary={menu.text}
-              className={`${
-                location.pathname === menu.path ? "active-link" : ""
-              }`}
-            />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
-  </Box>
-</Drawer>
+      {/* Mobile Drawer Menu */}
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((menu) => (
+              <ListItem key={menu.text} disablePadding>
+                <ListItemButton component={Link} to={menu.path}>
+                  <ListItemText
+                    primary={menu.text}
+                    className={`${
+                      location.pathname === menu.path ? "active-link" : ""
+                    }`}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
     </nav>
   );
 };
